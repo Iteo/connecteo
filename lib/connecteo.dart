83e05d0d1e408bfc0dc1dede296c62c496367e7a
Mateusz Filipek <mateuszfilipek2000@gmail.com>
2023-06-10 18:45:14 +0200

    chore: update connectivity plus dependency to newest version

--- lib/connecteo.dart
+++ lib/connecteo.dart
@@ -1,3 +1,5 @@
+/// Connecteo is a library that allows you to check the internet connection
+/// and track changes in the connection status.
 library connecteo;
 
 export 'src/connection_checker.dart';
